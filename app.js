72c4fe13615363c1ba8957dafb9531f356a12349
JerinBoyd <maxers@mac.com>
2018-01-31 20:31:44 -0500

    first box toggles red

---
+++ app.js
@@ -0,0 +1,20 @@
+const resetBtn = document.querySelector('#reset');
+const box = document.querySelector('.box');
+
+//eventListeners
+box.addEventListener('click', toggleRed);
+resetBtn.addEventListener('click', clearGrid);
+
+//functions
+
+function toggleRed(e){
+    const element = e.target;
+    element.classList.toggle('red');
+
+}
+
+function clearGrid(e){
+    console.log('clearing the grid');
+
+
+}

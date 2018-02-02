150e1297483b2f22eede6d8962f3b1c265c701aa
JerinBoyd <maxers@mac.com>
2018-02-01 19:24:59 -0500

    cleared and readded a dynamic grin to the main portion of our application

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const resetBtn = document.querySelector('#reset');
 const boxes = document.querySelectorAll('.box');
 const gridSizeForm = document.querySelector('#grid-size');
+const main = document.querySelector('main');
 
 //eventListeners
 boxes.forEach(function(box, index){
@@ -29,4 +30,14 @@ function clearGrid(e){
 function changeGrid(e){
     e.preventDefault();
     const gridSizeValue = +gridSizeForm.querySelector('input:checked').value;
+    const grid = makeGrid(gridSizeValue);
+    main.innerHTML = '';
+    main.appendChild(grid);
+}
+
+function makeGrid(size){
+    const element = document.createElement('h1');
+    element.textContent = 'GRID!!!!!!!';
+    return element;
+
 }
